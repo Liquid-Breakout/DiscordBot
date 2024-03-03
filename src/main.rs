06f9807968d49215d11132354f672b3c5c11107c
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-03-03 13:51:02 +0700

    err

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,14 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
     // They are many errors that can occur, so we only handle the ones we want to customize
     // and forward the rest to the default handler
     match error {
-        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start DiscordBot: {:?}", error),
         poise::FrameworkError::Command { error, ctx, .. } => {
-            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            println!("Error in command `{}`: {:?}", ctx.command().name, error);
+            let _ = ctx.say(format!("Error in command `{}`: {:?}", ctx.command().name, error)).await;
+        }
+        poise::FrameworkError::ArgumentParse { error, ctx, .. } => {
+            println!("Error while parsing argument(s) for command `{}`: {:?}", ctx.command().name, error);
+            let _ = ctx.say(format!("Error while parsing argument(s) for command `{}`: {:?}", ctx.command().name, error)).await;
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
